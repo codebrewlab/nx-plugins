705eb15fa4c11bed9a8ea2b1e7b0263f17257e39
tienne <tienne89@gmail.com>
2024-08-28 19:34:59 +0900

    feat: generators nx-ncc:application init

---
+++ packages/nx-ncc/src/generators/application/generator.ts
@@ -0,0 +1,17 @@
+import { addProjectConfiguration, formatFiles, generateFiles, Tree } from '@nx/devkit';
+import * as path from 'path';
+import { ApplicationGeneratorSchema } from './schema';
+
+export async function applicationGenerator(tree: Tree, options: ApplicationGeneratorSchema) {
+  const projectRoot = `libs/${options.name}`;
+  addProjectConfiguration(tree, options.name, {
+    root: projectRoot,
+    projectType: 'library',
+    sourceRoot: `${projectRoot}/src`,
+    targets: {},
+  });
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  await formatFiles(tree);
+}
+
+export default applicationGenerator;
